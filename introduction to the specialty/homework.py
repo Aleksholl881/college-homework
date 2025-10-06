8db4bf51e21663c33a19c8474b7bdbfb20612974
aleksandr kholodinin <70773838+Aleksholl881@users.noreply.github.com>
2025-10-06 21:23:39 +0300

    Add files via upload

---
+++ introduction to the specialty/homework.py
@@ -0,0 +1,9 @@
+integer_number = 42
+fractional_number = 3.14159
+line = "Привет, мир!"
+boolean_value = True
+
+print("Целое число:", integer_number, "| Тип:", type(integer_number))
+print("Дробное число:", fractional_number, "| Тип:", type(fractional_number))
+print("Строка:", line, "| Тип:", type(line))
+print("Булево значение:", boolean_value, "| Тип:", type(boolean_value))
